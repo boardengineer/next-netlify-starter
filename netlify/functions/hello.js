9e6d3531107d201f283136b22ab6bf8ff52bd207
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-07-13 14:37:20 -0700

    redirect test

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,9 @@
 exports.handler = async function (event, context) {
 	console.log("hello world")
 	return {
-		statusCode: 200,
-		body: "hello",
+		//statusCode: 200,
+		//body: "hello",
+		statusCode: 302,
+		"Location": "google.com",
 	}
 }
