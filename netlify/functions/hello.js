82616aeeb93c57ebf1c55273795bdde52fa9116f
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-07-13 18:57:12 -0700

    try to get query params

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,11 +1,11 @@
 exports.handler = async function (event, context) {
 	console.log("hello world")
 	return {
-		//statusCode: 200,
-		//body: "hello",
-		statusCode: 302,
-		headers: {
-		      Location: "https://www.google.com/"
-      		},
+		statusCode: 200,
+		body: event,
+		//statusCode: 302,
+		//headers: {
+		//      Location: "https://www.google.com/"
+      		//},
 	}
 }
