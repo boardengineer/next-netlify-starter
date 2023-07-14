621ff06d89cbea3e9c9ecc7a48bdf33e2acbc48e
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-07-13 21:22:28 -0700

    jwt

---
+++ netlify/functions/jwt.js
@@ -0,0 +1,21 @@
+exports.handler = async function (event, context) {
+
+	var uri = "https://id.twitch.tv/oauth2/token"
+
+	var token = event.queryStringParameters.access_token
+
+	return fetch(uri, {
+		method: "GET",
+		headers: {
+			"Authorization": "Bearer" + token
+		}
+	})
+	.then((response) => response.json())
+	.then((json) => {
+			console.log(JSON.stringify(json))
+			return {
+				statusCode: 200,
+				body: "Test Body",
+			};
+		})
+}
