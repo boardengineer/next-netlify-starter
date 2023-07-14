ee272a28a55b89a1bb245beb227b67fda5b1f817
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-07-13 19:01:33 -0700

    display code

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,7 +2,7 @@ exports.handler = async function (event, context) {
 	console.log("hello world")
 	return {
 		statusCode: 200,
-		body: event.queryStringParameters,
+		body: event.queryStringParameters.code,
 		//statusCode: 302,
 		//headers: {
 		//      Location: "https://www.google.com/"
