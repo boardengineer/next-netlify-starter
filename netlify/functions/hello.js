dc97fc3838a4e0b44c4c4640480040d24e6a1dcd
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-07-13 19:31:40 -0700

    test?

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,12 +1,30 @@
 exports.handler = async function (event, context) {
 	console.log("hello world")
+
+	var client_id = processs.env.CLIENT_ID
+	var client_secret = processs.env.CLIENT_SECRET
+
 	var code = event.queryStringParameters.code
-	return {
+	var redirect_uri = "localhost:31419"
+
+	var uri = "https://id.twitch.tv/oauth2/token"
+
+	var body = "client_id=" + client_id + "&client_secret=" + client_secret + "&code=" + code + "&grant_type=authorization_code&redirect_uri=http://localhost:31419"
+
+	return fetch(uri, {
+		method: "POST",
+		body: body,
+		headers: {
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+	.then((response) => response.json())
+	.then((json) => {
 		statusCode: 200,
-		body: code,
+		body: json,
 		//statusCode: 302,
 		//headers: {
 		//      Location: "https://www.google.com/"
       		//},
-	}
+	})
 }
