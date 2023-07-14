c25530fd2104dcfc5f59897ddd2be1bc5afd2af3
boardengineer <77598527+boardengineer@users.noreply.github.com>
2023-07-13 19:05:57 -0700

    is this how javascript works?

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,9 @@
 exports.handler = async function (event, context) {
 	console.log("hello world")
+	var code = event.queryStringParameters.code
 	return {
 		statusCode: 200,
-		body: event.queryStringParameters.code,
+		body: code,
 		//statusCode: 302,
 		//headers: {
 		//      Location: "https://www.google.com/"
